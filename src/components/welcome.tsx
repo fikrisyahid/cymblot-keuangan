eb7454b3d1768890f412ed3c838307b8d90f74f6
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-05-26 18:17:23 +0700

    fix: unstable cache bug

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,23 +1,23 @@
 import prisma from "@/app/db/init";
 import SaldoCard from "@/components/saldo-card";
 import { currentUser } from "@clerk/nextjs/server";
-import { Button, Flex, Title } from "@mantine/core";
+import { Flex, Title } from "@mantine/core";
 import { unstable_cache } from "next/cache";
 
 export default async function Welcome() {
   const user = await currentUser();
 
-  const transaksiUser = unstable_cache(
+  const transaksiUserCache = unstable_cache(
     async () =>
       await prisma.transaksi.findMany({
         where: {
-          user: {
-            email: user?.emailAddresses[0].emailAddress,
-          },
+          email: user?.emailAddresses[0].emailAddress,
         },
       }),
     ["revalidateWelcome"]
   );
+
+  const transaksiUser = await transaksiUserCache();
 
   return (
     <>
