1f943845d2d066f720e1e90851b80605462b2410
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-05-26 19:25:59 +0700

    progress: detail page (preprocess table data)

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,19 +1,45 @@
 import BaseTable from "@/components/base-table";
+import { currentUser } from "@clerk/nextjs/server";
+import prisma from "../db/init";
+import PrettyJSON from "@/components/pretty-json";
 
-export default function Page() {
+export default async function Page() {
+  const user = await currentUser();
+
+  const transaksiUser = await prisma.transaksi.findMany({
+    where: {
+      email: user?.emailAddresses[0].emailAddress,
+    },
+    include: {
+      sumber: true,
+      tujuan: true,
+    },
+  });
+
+  const dataForTable = transaksiUser.map((transaksi, index) => ({
+    No: index + 1,
+    Tanggal: transaksi.tanggal.toLocaleDateString(),
+    Keterangan: transaksi.keterangan,
+    Jenis: transaksi.jenis,
+    Sumber: transaksi.sumber?.nama || "-",
+    Tujuan: transaksi.tujuan?.nama || "-",
+    Nominal: transaksi.nominal,
+  }));
+
   return (
     <>
+      <PrettyJSON text={transaksiUser} />
       <BaseTable
         columns={[
-          { accessor: "id" },
-          { accessor: "name" },
-          { accessor: "bornIn", width: 150 },
-          { accessor: "party" },
+          { accessor: "No" },
+          { accessor: "Tanggal" },
+          { accessor: "Keterangan" },
+          { accessor: "Jenis" },
+          { accessor: "Sumber" },
+          { accessor: "Tujuan" },
+          { accessor: "Nominal" },
         ]}
-        records={[
-          { id: 1, name: "Joe Biden", bornIn: 1942, party: "Democratic" },
-          { id: 1, name: "Joe awd", bornIn: 1942, party: "Democratic" },
-        ]}
+        records={dataForTable}
       />
     </>
   );
