8adfeccc56ea3e6d5e93e9906c3263c138505fa1
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-05-26 18:58:16 +0700

    update: text color config & welcome bg colors

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -3,6 +3,7 @@ import SaldoCard from "@/components/saldo-card";
 import { currentUser } from "@clerk/nextjs/server";
 import { Flex, Title } from "@mantine/core";
 import { unstable_cache } from "next/cache";
+import { TEXT_COLOR } from "@/config";
 
 export default async function Welcome() {
   const user = await currentUser();
@@ -21,15 +22,15 @@ export default async function Welcome() {
 
   return (
     <>
-      <Title>Halo {user?.fullName}</Title>
+      <Title style={{ color: TEXT_COLOR }}>Halo {user?.fullName}</Title>
       <Flex gap="sm">
         <SaldoCard
-          backgroundColor="#615EFC"
+          backgroundColor="#38598b"
           title="Total saldo"
           text="Rp500,000.00"
         />
-        <SaldoCard backgroundColor="#FFA62F" title="Bank" text="Rp500,000.00" />
-        <SaldoCard backgroundColor="#D2649A" title="Cash" text="Rp500,000.00" />
+        <SaldoCard backgroundColor="#5177b0" title="Bank" text="Rp500,000.00" />
+        <SaldoCard backgroundColor="#72aad4" title="Cash" text="Rp500,000.00" />
       </Flex>
     </>
   );
