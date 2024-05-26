a992d5048c83f51aa694eed6b39108b019edb0d9
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-05-26 17:26:04 +0700

    update: base table component

---
+++ src/components/base-table.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { DataTable } from "mantine-datatable";
+
+export default function BaseTable({
+  columns,
+  records,
+}: {
+  columns: any[];
+  records: any[];
+}) {
+  return (
+    <>
+      <DataTable
+        withTableBorder
+        borderRadius="md"
+        columns={columns}
+        records={records}
+      />
+    </>
+  );
+}
