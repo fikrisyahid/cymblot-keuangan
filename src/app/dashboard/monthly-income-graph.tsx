11e528515781ce4cbaeabeeb8bdf1b560b7f6b8f
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-06-13 07:08:27 +0700

    progress: dashboard page

---
+++ src/app/dashboard/monthly-income-graph.tsx
@@ -0,0 +1,26 @@
+import { BarChart } from "@mantine/charts";
+
+export default function MonthlyIncomeGraph() {
+  return (
+    <BarChart
+      h={300}
+      data={[
+        { name: "Jan", value: 1000 },
+        { name: "Feb", value: 2000 },
+        { name: "Mar", value: 1500 },
+        { name: "Apr", value: 3000 },
+        { name: "May", value: 2500 },
+        { name: "Jun", value: 3500 },
+        { name: "Jul", value: 4000 },
+        { name: "Aug", value: 3000 },
+        { name: "Sep", value: 3500 },
+        { name: "Oct", value: 2000 },
+        { name: "Nov", value: 2500 },
+        { name: "Dec", value: 3000 },
+      ]}
+      dataKey="name"
+      series={[{ name: "value", color: "violet.6" }]}
+      tickLine="y"
+    />
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,57 @@
 import MainCard from "@/components/main-card";
 import Welcome, { WelcomeSkeleton } from "./welcome";
 import { Suspense } from "react";
+import MonthlyCostGraph from "./monthly-cost-graph";
+import { Stack, Text } from "@mantine/core";
+import { TEXT_COLOR } from "@/config";
+import MonthlyIncomeGraph from "./monthly-income-graph";
+import { User, currentUser } from "@clerk/nextjs/server";
+import prisma from "../db/init";
 
-export default function Page() {
+export default async function Page() {
+  const user = await currentUser();
+
+  const transaksiUser = await prisma.transaksi.findMany({
+    where: {
+      email: user?.emailAddresses[0].emailAddress,
+    },
+  });
+
   return (
-    <MainCard>
-      <Suspense fallback={<WelcomeSkeleton />}>
-        <Welcome />
-      </Suspense>
-    </MainCard>
+    <Stack>
+      <MainCard>
+        <Suspense fallback={<WelcomeSkeleton />}>
+          <Welcome user={user as User} transaksiUser={transaksiUser} />
+        </Suspense>
+      </MainCard>
+      <MainCard transparent noPadding row>
+        <MainCard fullWidth>
+          <Text
+            fw={700}
+            size="xl"
+            c={TEXT_COLOR}
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Pemasukan Bulan Ini
+          </Text>
+          <MonthlyCostGraph />
+        </MainCard>
+        <MainCard fullWidth>
+          <Text
+            fw={700}
+            size="xl"
+            c={TEXT_COLOR}
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Pengeluaran Bulan Ini
+          </Text>
+          <MonthlyIncomeGraph />
+        </MainCard>
+      </MainCard>
+    </Stack>
   );
 }
