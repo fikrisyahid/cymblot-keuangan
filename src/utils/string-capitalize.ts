398b1b53ad509fc53721b8a21908db988e6c30c5
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-06-15 12:24:09 +0700

    fix: string capitalize util bug

--- src/utils/string-capitalize.ts
+++ src/utils/string-capitalize.ts
@@ -1,3 +1,3 @@
 export default function stringCapitalize(str: string): string {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
