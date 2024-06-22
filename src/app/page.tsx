a6b56f5a5d8111760d17ec0d2557f3d68fc8566e
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-06-22 10:21:23 +0700

    fix: a better way ro redirect to /dashboard from /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const redirectScript = setInterval(() => {
-      router.push("/dashboard");
-    }, 100);
-
-    return () => {
-      clearInterval(redirectScript);
-    };
-  }, [router]);
-
-  return <></>;
+  redirect("/dashboard");
 }
