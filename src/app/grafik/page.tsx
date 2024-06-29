ff234474ab1a6d48f06bb261508372fde1b8c06b
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-06-29 21:13:06 +0700

    fix: turn off grafik admin (for now)

--- src/app/grafik/page.tsx
+++ src/app/grafik/page.tsx
@@ -1,25 +1,20 @@
 import MainCard from "@/components/main-card";
-import { MONITORED_EMAIL, TEXT_COLOR } from "@/config";
+import { TEXT_COLOR } from "@/config";
 import { currentUser } from "@clerk/nextjs/server";
 import { Title } from "@mantine/core";
 import prisma from "../db/init";
 import MainBarChart from "./c-barchart";
 import dayjs from "dayjs";
-import isAdmin from "@/utils/is-admin";
 
 async function getUserTransactions(email: string) {
-  const loggedInAsAdmin = isAdmin(email);
-  const where = loggedInAsAdmin
-    ? { OR: [{ email: { in: MONITORED_EMAIL } }, { email }] }
-    : { email };
   const [transaksiUser, daftarSumber, daftarTujuan] = await Promise.all([
     prisma.transaksi.findMany({
-      where,
+      where: { email },
       include: { sumber: true, tujuan: true, bankName: true },
       orderBy: { tanggal: "desc" },
     }),
-    prisma.sumber.findMany({ where, orderBy: { createdAt: "asc" } }),
-    prisma.tujuan.findMany({ where, orderBy: { createdAt: "asc" } }),
+    prisma.sumber.findMany({ where: { email }, orderBy: { createdAt: "asc" } }),
+    prisma.tujuan.findMany({ where: { email }, orderBy: { createdAt: "asc" } }),
   ]);
 
   return { transaksiUser, daftarSumber, daftarTujuan };
