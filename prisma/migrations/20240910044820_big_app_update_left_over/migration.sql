0a281aafb6c8724c85e1fb313dccdda7ad8725cf
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-09-10 12:15:24 +0700

    chore: big app update 1.3 init

---
+++ prisma/migrations/20240910044820_big_app_update_left_over/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Transaction" ADD COLUMN     "categoryId" TEXT,
+ADD COLUMN     "walletId" TEXT;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_walletId_fkey" FOREIGN KEY ("walletId") REFERENCES "Wallet"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
