d56bd50b596518d96c0a8fc9080fae49810805a0
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-09-15 22:55:23 +0700

    chore: reset whole project and migrate to mantine

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-import { ThemeToggle } from '@/components/theme-toggle';
-import { Button } from '@/components/ui/button';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <main>
-      <h1>Hello, world!</h1>
-      <Button>Hello</Button>
-      <ThemeToggle />
-    </main>
-  );
+  redirect('/dashboard');
 }
