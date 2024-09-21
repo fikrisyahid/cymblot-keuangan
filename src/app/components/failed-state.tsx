6b9bed22a835c70759d8be15058a55058371959c
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-09-21 11:49:33 +0700

    fix (ui): failed state component layout fix + more informative

--- src/app/components/failed-state.tsx
+++ src/app/components/failed-state.tsx
@@ -1,27 +1,34 @@
 'use client';
 
 import { Button } from '@mantine/core';
-import { useRouter } from 'next/navigation';
+import { BUTTON_BASE_COLOR } from '@/config/color';
 import MainCard from './main-card';
 
 export default function FailedState() {
-  const router = useRouter();
-
   const refreshPage = () => {
-    router.refresh();
+    window.location.reload();
   };
 
   return (
     <MainCard>
-      <div className="text-center">
-        <h1 className="text-4xl font-bold text-red-600">
-          Gagal membuka halaman
-        </h1>
-        <div className="flex flex-row gap-2">
-          <p className="mt-4 text-lg text-gray-700">Silahkan ulangi</p>
-          <Button onClick={refreshPage}>Refresh halaman</Button>
-        </div>
-      </div>
+      <h1 className="text-center text-4xl font-bold text-red-600">
+        Gagal membuka halaman
+      </h1>
+      <p className="text-center text-lg text-gray-700">
+        Silahkan hubungi{' '}
+        <a
+          href="https://github.com/fs3120"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500"
+        >
+          Admin
+        </a>{' '}
+        atau ulangi
+      </p>
+      <Button color={BUTTON_BASE_COLOR} onClick={refreshPage}>
+        Refresh halaman
+      </Button>
     </MainCard>
   );
 }
