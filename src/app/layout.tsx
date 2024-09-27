5f801dea3d3a6055d4290f66b47dbddede529528
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-09-28 03:31:44 +0700

    feature: better app icon for mobile device

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,8 @@ export default function RootLayout({
       <html lang="en">
         <head>
           <ColorSchemeScript />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         </head>
         <body className="antialiased">
           <MantineProvider
