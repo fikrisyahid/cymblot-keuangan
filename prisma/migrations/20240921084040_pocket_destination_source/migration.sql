7e55267234056f76f55725b13762e2c5bc19d27f
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-09-27 11:18:43 +0700

    db: delete migrations (error production deploy)

--- prisma/migrations/20240921084040_pocket_destination_source/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "Transaction" ADD COLUMN     "pocketDestinationId" TEXT,
-ADD COLUMN     "pocketSourceId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_pocketSourceId_fkey" FOREIGN KEY ("pocketSourceId") REFERENCES "Pocket"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_pocketDestinationId_fkey" FOREIGN KEY ("pocketDestinationId") REFERENCES "Pocket"("id") ON DELETE CASCADE ON UPDATE CASCADE;
