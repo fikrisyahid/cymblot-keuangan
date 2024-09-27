17e57330841679e78c174160b58b34dce09fcb64
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-09-27 16:15:03 +0700

    fix (ui): graph page remove spoiler + full width filter

--- src/app/graph/chart.tsx
+++ src/app/graph/chart.tsx
@@ -2,12 +2,12 @@
 
 import { BarChart } from '@mantine/charts';
 import {
+  Alert,
   Button,
   Checkbox,
   NumberInput,
   SegmentedControl,
   Select,
-  Spoiler,
   Stack,
   Text,
   TextInput,
@@ -15,6 +15,8 @@ import {
 import dayjs from 'dayjs';
 import 'dayjs/locale/id';
 import utc from 'dayjs/plugin/utc';
+import { IconInfoCircle } from '@tabler/icons-react';
+import Link from 'next/link';
 import timezone from 'dayjs/plugin/timezone';
 import { useMemo, useState } from 'react';
 import { Category, Pocket } from '@prisma/client';
@@ -171,274 +173,310 @@ export default function DetailChart({
   );
 
   return (
-    <Spoiler
-      maxHeight={300}
-      showLabel="Lihat lebih lanjut"
-      hideLabel="Sembunyikan"
-    >
-      <Stack>
-        <div className="flex flex-col gap-2 sm:flex-row">
-          <SegmentedControl
-            value={filter.mode}
+    <Stack>
+      <div className="flex flex-col gap-2 sm:flex-row">
+        <SegmentedControl
+          value={filter.mode}
+          onChange={(value) =>
+            setFilter((prev) => ({
+              ...prev,
+              mode: value as IChartFilter['mode'],
+            }))
+          }
+          data={[
+            { label: 'Jarak', value: 'range' },
+            { label: 'Hari', value: 'day' },
+            { label: 'Bulan', value: 'month' },
+            { label: 'Tahun', value: 'year' },
+          ]}
+        />
+        {filter.mode === 'day' && (
+          <Select
+            value={filter.day.toString()}
+            data={Array.from(
+              { length: dayjs().month(filter.month).daysInMonth() },
+              (_, i) => ({
+                value: (i + 1).toString(),
+                label: (i + 1).toString(),
+              }),
+            )}
             onChange={(value) =>
               setFilter((prev) => ({
                 ...prev,
-                mode: value as IChartFilter['mode'],
+                day: parseInt(value as string, 10),
               }))
             }
-            data={[
-              { label: 'Jarak', value: 'range' },
-              { label: 'Hari', value: 'day' },
-              { label: 'Bulan', value: 'month' },
-              { label: 'Tahun', value: 'year' },
-            ]}
           />
-          {filter.mode === 'day' && (
-            <Select
-              value={filter.day.toString()}
-              data={Array.from(
-                { length: dayjs().month(filter.month).daysInMonth() },
-                (_, i) => ({
-                  value: (i + 1).toString(),
-                  label: (i + 1).toString(),
-                }),
-              )}
-              onChange={(value) =>
-                setFilter((prev) => ({
-                  ...prev,
-                  day: parseInt(value as string, 10),
-                }))
-              }
-            />
+        )}
+        {(filter.mode === 'day' || filter.mode === 'month') && (
+          <Select
+            value={filter.month.toString()}
+            data={Array.from({ length: 12 }, (_, i) => ({
+              value: i.toString(),
+              label: dayjs().month(i).format('MMMM'),
+            }))}
+            onChange={(value) =>
+              setFilter((prev) => ({
+                ...prev,
+                month: parseInt(value as string, 10),
+              }))
+            }
+          />
+        )}
+        {(filter.mode === 'day' ||
+          filter.mode === 'month' ||
+          filter.mode === 'year') && (
+          <Select
+            value={filter.year.toString()}
+            data={Array.from(
+              { length: dayjs().year() - oldestTransactionDate.year() + 1 },
+              (_, i) => ({
+                value: (oldestTransactionDate.year() + i).toString(),
+                label: (oldestTransactionDate.year() + i).toString(),
+              }),
+            )}
+            onChange={(value) =>
+              setFilter((prev) => ({
+                ...prev,
+                year: parseInt(value as string, 10),
+              }))
+            }
+          />
+        )}
+      </div>
+      <div className="flex flex-col gap-4 sm:flex-row">
+        <Stack gap={0} w="100%">
+          <Text fw={500}>Filter keterangan</Text>
+          <TextInput
+            placeholder="Masukkan keterangan yang ingin dicari"
+            value={filter.information}
+            onChange={(e) =>
+              handleChange({ information: e.currentTarget.value })
+            }
+            style={{
+              width: '350px',
+            }}
+          />
+        </Stack>
+        <Stack>
+          <Text fw={500} mb={-10}>
+            Filter jenis
+          </Text>
+          <Checkbox
+            checked={filter.type.includes('DEPOSIT')}
+            label="PEMASUKAN"
+            onChange={(e) =>
+              handleChange({
+                type: e.currentTarget.checked
+                  ? [...filter.type, 'DEPOSIT']
+                  : filter.type.filter((f) => f !== 'DEPOSIT'),
+              })
+            }
+          />
+          <Checkbox
+            checked={filter.type.includes('WITHDRAW')}
+            label="PENGELUARAN"
+            onChange={(e) =>
+              handleChange({
+                type: e.currentTarget.checked
+                  ? [...filter.type, 'WITHDRAW']
+                  : filter.type.filter((f) => f !== 'WITHDRAW'),
+              })
+            }
+          />
+          <Checkbox
+            checked={filter.type.includes('TRANSFER')}
+            label="TRANSFER"
+            onChange={(e) =>
+              handleChange({
+                type: e.currentTarget.checked
+                  ? [...filter.type, 'TRANSFER']
+                  : filter.type.filter((f) => f !== 'TRANSFER'),
+              })
+            }
+          />
+          {filter.type.length > 0 && (
+            <Button
+              color={BUTTON_BASE_COLOR}
+              onClick={() => handleChange({ type: [] })}
+            >
+              Reset
+            </Button>
           )}
-          {(filter.mode === 'day' || filter.mode === 'month') && (
-            <Select
-              value={filter.month.toString()}
-              data={Array.from({ length: 12 }, (_, i) => ({
-                value: i.toString(),
-                label: dayjs().month(i).format('MMMM'),
-              }))}
-              onChange={(value) =>
-                setFilter((prev) => ({
-                  ...prev,
-                  month: parseInt(value as string, 10),
-                }))
+        </Stack>
+        <Stack w="100%">
+          <Text fw={500} mb={-10}>
+            Filter nominal
+          </Text>
+          <NumberInput
+            label="Batas bawah"
+            description="Filter transaksi dengan nominal lebih besar dari atau sama dengan"
+            thousandSeparator=","
+            placeholder="Masukkan batas bawah"
+            value={filter.value.min}
+            prefix="Rp"
+            allowNegative={false}
+            onChange={(e) =>
+              handleChange({ value: { ...filter.value, min: e } })
+            }
+          />
+          <NumberInput
+            label="Batas atas"
+            description="Filter transaksi dengan nominal lebih kecil dari atau sama dengan"
+            thousandSeparator=","
+            placeholder="Masukkan batas atas"
+            value={filter.value.max}
+            prefix="Rp"
+            allowNegative={false}
+            onChange={(e) =>
+              handleChange({ value: { ...filter.value, max: e } })
+            }
+          />
+          <NumberInput
+            label="Sama dengan"
+            description="Filter transaksi dengan nominal sama dengan"
+            thousandSeparator=","
+            placeholder="Masukkan nominal"
+            value={filter.value.equal}
+            prefix="Rp"
+            allowNegative={false}
+            onChange={(e) =>
+              handleChange({ value: { ...filter.value, equal: e } })
+            }
+          />
+          {Object.values(filter.value).some((v) => v !== '') && (
+            <Button
+              color={BUTTON_BASE_COLOR}
+              onClick={() =>
+                handleChange({
+                  value: {
+                    min: '',
+                    max: '',
+                    equal: '',
+                  },
+                })
               }
-            />
+            >
+              Reset
+            </Button>
           )}
-          {(filter.mode === 'day' ||
-            filter.mode === 'month' ||
-            filter.mode === 'year') && (
-            <Select
-              value={filter.year.toString()}
-              data={Array.from(
-                { length: dayjs().year() - oldestTransactionDate.year() + 1 },
-                (_, i) => ({
-                  value: (oldestTransactionDate.year() + i).toString(),
-                  label: (oldestTransactionDate.year() + i).toString(),
-                }),
-              )}
-              onChange={(value) =>
-                setFilter((prev) => ({
-                  ...prev,
-                  year: parseInt(value as string, 10),
-                }))
-              }
-            />
+        </Stack>
+        <Stack w="100%">
+          <Text fw={500} mb={-10}>
+            Filter kategori
+          </Text>
+          {categories.length === 0 && (
+            <Alert
+              variant="filled"
+              color="indigo"
+              title="Info"
+              icon={<IconInfoCircle />}
+              p="xs"
+              w="100%"
+            >
+              <Stack align="start">
+                <Text c="white">
+                  Anda belum memiliki kategori. Silakan tambahkan kategori
+                  terlebih dahulu
+                </Text>
+                <Button component={Link} href="/category" color="teal">
+                  Tambah Kategori
+                </Button>
+              </Stack>
+            </Alert>
           )}
-        </div>
-        <div className="flex flex-col gap-4 sm:flex-row">
-          <Stack gap={0}>
-            <Text fw={500}>Filter keterangan</Text>
-            <TextInput
-              placeholder="Masukkan keterangan yang ingin dicari"
-              value={filter.information}
-              onChange={(e) =>
-                handleChange({ information: e.currentTarget.value })
-              }
-              style={{
-                width: '350px',
-              }}
-            />
-          </Stack>
-          <Stack>
-            <Text fw={500} mb={-10}>
-              Filter jenis
-            </Text>
+          {categories.map((category) => (
             <Checkbox
-              checked={filter.type.includes('DEPOSIT')}
-              label="PEMASUKAN"
+              key={category.id}
+              checked={filter.category.includes(category.id)}
+              label={category.name}
               onChange={(e) =>
                 handleChange({
-                  type: e.currentTarget.checked
-                    ? [...filter.type, 'DEPOSIT']
-                    : filter.type.filter((f) => f !== 'DEPOSIT'),
+                  category: e.currentTarget.checked
+                    ? [...filter.category, category.id]
+                    : filter.category.filter((f) => f !== category.id),
                 })
               }
             />
+          ))}
+          {filter.category.length > 0 && (
+            <Button
+              color={BUTTON_BASE_COLOR}
+              onClick={() =>
+                handleChange({
+                  category: [],
+                })
+              }
+            >
+              Reset
+            </Button>
+          )}
+        </Stack>
+        <Stack w="100%">
+          <Text fw={500} mb={-10}>
+            Filter kantong
+          </Text>
+          {pockets.length === 0 && (
+            <Alert
+              variant="filled"
+              color="indigo"
+              title="Info"
+              icon={<IconInfoCircle />}
+              p="xs"
+              w="100%"
+            >
+              <Stack align="start">
+                <Text c="white">
+                  Anda belum memiliki kantong. Silakan tambahkan kantong
+                  terlebih dahulu
+                </Text>
+                <Button component={Link} href="/pocket" color="teal">
+                  Tambah Kantong
+                </Button>
+              </Stack>
+            </Alert>
+          )}
+          {pockets.map((pocket) => (
             <Checkbox
-              checked={filter.type.includes('WITHDRAW')}
-              label="PENGELUARAN"
+              key={pocket.id}
+              checked={filter.pocket.includes(pocket.id)}
+              label={pocket.name}
               onChange={(e) =>
                 handleChange({
-                  type: e.currentTarget.checked
-                    ? [...filter.type, 'WITHDRAW']
-                    : filter.type.filter((f) => f !== 'WITHDRAW'),
+                  pocket: e.currentTarget.checked
+                    ? [...filter.pocket, pocket.id]
+                    : filter.pocket.filter((f) => f !== pocket.id),
                 })
               }
             />
-            <Checkbox
-              checked={filter.type.includes('TRANSFER')}
-              label="TRANSFER"
-              onChange={(e) =>
+          ))}
+          {filter.pocket.length > 0 && (
+            <Button
+              color={BUTTON_BASE_COLOR}
+              onClick={() =>
                 handleChange({
-                  type: e.currentTarget.checked
-                    ? [...filter.type, 'TRANSFER']
-                    : filter.type.filter((f) => f !== 'TRANSFER'),
+                  pocket: [],
                 })
               }
-            />
-            {filter.type.length > 0 && (
-              <Button
-                color={BUTTON_BASE_COLOR}
-                onClick={() => handleChange({ type: [] })}
-              >
-                Reset
-              </Button>
-            )}
-          </Stack>
-          <Stack>
-            <Text fw={500} mb={-10}>
-              Filter nominal
-            </Text>
-            <NumberInput
-              label="Batas bawah"
-              description="Filter transaksi dengan nominal lebih besar dari atau sama dengan"
-              thousandSeparator=","
-              placeholder="Masukkan batas bawah"
-              value={filter.value.min}
-              prefix="Rp"
-              allowNegative={false}
-              onChange={(e) =>
-                handleChange({ value: { ...filter.value, min: e } })
-              }
-            />
-            <NumberInput
-              label="Batas atas"
-              description="Filter transaksi dengan nominal lebih kecil dari atau sama dengan"
-              thousandSeparator=","
-              placeholder="Masukkan batas atas"
-              value={filter.value.max}
-              prefix="Rp"
-              allowNegative={false}
-              onChange={(e) =>
-                handleChange({ value: { ...filter.value, max: e } })
-              }
-            />
-            <NumberInput
-              label="Sama dengan"
-              description="Filter transaksi dengan nominal sama dengan"
-              thousandSeparator=","
-              placeholder="Masukkan nominal"
-              value={filter.value.equal}
-              prefix="Rp"
-              allowNegative={false}
-              onChange={(e) =>
-                handleChange({ value: { ...filter.value, equal: e } })
-              }
-            />
-            {Object.values(filter.value).some((v) => v !== '') && (
-              <Button
-                color={BUTTON_BASE_COLOR}
-                onClick={() =>
-                  handleChange({
-                    value: {
-                      min: '',
-                      max: '',
-                      equal: '',
-                    },
-                  })
-                }
-              >
-                Reset
-              </Button>
-            )}
-          </Stack>
-          <Stack>
-            <Text fw={500} mb={-10}>
-              Filter kategori
-            </Text>
-            {categories.map((category) => (
-              <Checkbox
-                key={category.id}
-                checked={filter.category.includes(category.id)}
-                label={category.name}
-                onChange={(e) =>
-                  handleChange({
-                    category: e.currentTarget.checked
-                      ? [...filter.category, category.id]
-                      : filter.category.filter((f) => f !== category.id),
-                  })
-                }
-              />
-            ))}
-            {filter.category.length > 0 && (
-              <Button
-                color={BUTTON_BASE_COLOR}
-                onClick={() =>
-                  handleChange({
-                    category: [],
-                  })
-                }
-              >
-                Reset
-              </Button>
-            )}
-          </Stack>
-          <Stack>
-            <Text fw={500} mb={-10}>
-              Filter kantong
-            </Text>
-            {pockets.map((pocket) => (
-              <Checkbox
-                key={pocket.id}
-                checked={filter.pocket.includes(pocket.id)}
-                label={pocket.name}
-                onChange={(e) =>
-                  handleChange({
-                    pocket: e.currentTarget.checked
-                      ? [...filter.pocket, pocket.id]
-                      : filter.pocket.filter((f) => f !== pocket.id),
-                  })
-                }
-              />
-            ))}
-            {filter.pocket.length > 0 && (
-              <Button
-                color={BUTTON_BASE_COLOR}
-                onClick={() =>
-                  handleChange({
-                    pocket: [],
-                  })
-                }
-              >
-                Reset
-              </Button>
-            )}
-          </Stack>
-        </div>
-        <BarChart
-          h={300}
-          data={chartData}
-          dataKey="timePoint"
-          series={[
-            { name: 'withdraw', color: 'red.6' },
-            { name: 'deposit', color: 'teal.6' },
-            { name: 'transfer', color: 'violet.6' },
-          ]}
-          tickLine="y"
-        />
-      </Stack>
-    </Spoiler>
+            >
+              Reset
+            </Button>
+          )}
+        </Stack>
+      </div>
+      <BarChart
+        h={300}
+        data={chartData}
+        dataKey="timePoint"
+        series={[
+          { name: 'withdraw', color: 'red.6' },
+          { name: 'deposit', color: 'teal.6' },
+          { name: 'transfer', color: 'violet.6' },
+        ]}
+        tickLine="y"
+        withLegend
+        tooltipAnimationDuration={200}
+      />
+    </Stack>
   );
 }
