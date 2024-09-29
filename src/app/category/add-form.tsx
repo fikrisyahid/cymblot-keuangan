023a787123175294b8b777708077ac6a515dc7cb
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2024-09-29 10:15:02 +0700

    fix (ui): add form category & pocket update gap from 4 to 2

--- src/app/category/add-form.tsx
+++ src/app/category/add-form.tsx
@@ -72,7 +72,7 @@ export default function AddCategoryForm({
   };
 
   return (
-    <div className="flex flex-col sm:flex-row gap-4 justify-center">
+    <div className="flex flex-col sm:flex-row gap-2 justify-center">
       <Input
         name="name"
         placeholder="Nama kategori baru"
