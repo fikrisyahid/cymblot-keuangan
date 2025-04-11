b5681ed2c78a1cd9d63088d5a9fb4909887397f5
Muhammad Fikri Syahid <fikrisyahid14@gmail.com>
2025-04-11 20:36:52 +0700

    feature (ui): loading skeleton graph page

--- src/app/graph/components/chart/skeleton.tsx
+++ src/app/graph/components/chart/skeleton.tsx
@@ -1,5 +1,55 @@
-import LoadingSkeleton from '@/app/components/loading-skeleton';
+import { NumberFormatter, Skeleton, Stack, Text } from '@mantine/core';
 
 export default function DetailChartSkeleton() {
-  return <LoadingSkeleton />;
+  return (
+    <Stack>
+      <div className="flex flex-col sm:flex-row items-center sm:items-start gap-2">
+        <div className="flex flex-col sm:flex-row items-center sm:items-start gap-0 sm:gap-1 w-full sm:w-auto bg-primary p-2 rounded-md shadow-md">
+          <Text c="white" size="sm">
+            Selisih:{' '}
+          </Text>
+          <NumberFormatter
+            prefix="Rp "
+            value={0}
+            thousandSeparator
+            className="text-white text-sm font-bold"
+          />
+        </div>
+        <div className="flex flex-col sm:flex-row items-center sm:items-start gap-0 sm:gap-1 w-full sm:w-auto bg-[#4b9a41] p-2 rounded-md shadow-md">
+          <Text c="white" size="sm">
+            Pemasukan:{' '}
+          </Text>
+          <NumberFormatter
+            prefix="Rp "
+            value={0}
+            thousandSeparator
+            className="text-white text-sm font-bold"
+          />
+        </div>
+        <div className="flex flex-col sm:flex-row items-center sm:items-start gap-0 sm:gap-1 w-full sm:w-auto bg-red-500 p-2 rounded-md shadow-md">
+          <Text c="white" size="sm">
+            Pengeluaran:{' '}
+          </Text>
+          <NumberFormatter
+            prefix="Rp "
+            value={0}
+            thousandSeparator
+            className="text-white text-sm font-bold"
+          />
+        </div>
+        <div className="flex flex-col sm:flex-row items-center sm:items-start gap-0 sm:gap-1 w-full sm:w-auto bg-violet-600 p-2 rounded-md shadow-md">
+          <Text c="white" size="sm">
+            Transfer:{' '}
+          </Text>
+          <NumberFormatter
+            prefix="Rp "
+            value={0}
+            thousandSeparator
+            className="text-white text-sm font-bold"
+          />
+        </div>
+      </div>
+      <Skeleton height={300} />
+    </Stack>
+  );
 }
